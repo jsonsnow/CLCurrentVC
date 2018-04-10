0008b27a3d1b0ae75870cb6d407e6b2a986201d3
jsonsnow <1183010734@qq.com>
2018-04-10 23:56:11 +0800

    Initial Commit

---
+++ CLCurrentControllerTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLCurrentControllerTool
+//
+//  Created by chen liang on 2018/4/10.
+//  Copyright © 2018年 chen liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
